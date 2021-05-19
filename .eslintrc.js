7b778f3533cb4c787385b41970fc7e6e8c16ce11
Deniz <denizderezzed@gmail.com>
2021-05-19 07:15:45 +0200

    project init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/recommended', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 2],
+    semi: ['error', 'never'],
+    'space-before-function-paren': ['error', 'never'],
+    'vue/valid-v-slot': 'off'
   }
 }
