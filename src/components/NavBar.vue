53925bfc2e82d20088da5ee46ab66707cd30bee8
Deniz <denizderezzed@gmail.com>
2021-05-19 09:28:26 +0200

    creating components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar
+    app
+    color="#35495e"
+    dark
+    class=""
+  >
+    <div class="d-flex align-center mx-auto">
+      <v-img
+        alt="Vue Logo"
+        class="shrink mr-2"
+        contain
+        src="@/assets/logo.png"
+        transition="scale-transition"
+        width="40"
+      />
+      <div class="text-h5">
+        Vue-Todo
+      </div>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="todo-app">
+    todo
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
