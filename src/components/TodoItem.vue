38f7fd1a481d1fdaa84a5ff269acb3bdc01a14a5
Deniz <denizderezzed@gmail.com>
2021-05-19 15:37:22 +0200

    one more comment

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -107,8 +107,8 @@ export default {
 .slide-fade-leave-active {
   transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
 }
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
+.slide-fade-enter,
+.slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
