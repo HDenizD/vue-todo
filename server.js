5a591b624af83efc6ee9a51ef73efdf9e6d0867b
Deniz <denizderezzed@gmail.com>
2021-05-19 17:58:07 +0200

    added start script

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('./node_modules/express/index.js')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use('/public', express.static(__dirname + '/public'))
+app.use('/node', express.static(__dirname + '/node_modules'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port localhost:${port}`)
+})
